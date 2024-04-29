ff36c063ff95194e1bb05ef42b05052eaded421e
B45ittu <bittuelkiwar@gmail.com>
2024-04-29 19:28:17 +0530

    commit6

--- backend/leetcode/userData.js
+++ backend/leetcode/userData.js
@@ -1,9 +1,6 @@
-// userdata.js
-
 // const fetch = require('node-fetch');
 async function getAllUsernames() {
-  // Implement logic to fetch usernames or identifiers of all users from your website's database
-  // Example: return an array of usernames
+
   return ["Preeti_Desai07","gayatri_d12","Prasanna_Fuse","ayushdongardive","snehal_elkiwar","Ani2204"];
 }
        
--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import './leaderboard.css';
+import goldImage from '../assets/gold.png'; 
+import silverImage from '../assets/silver.png'; 
+import bronzeImage from '../assets/bronze.png'; 
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
@@ -18,12 +22,16 @@ const Leaderboard = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <ul>
+    <div className="leaderboard-container">
+      <h1 className="leaderboard-header">Leaderboard</h1>
+      <ul className="leaderboard-list">
         {leaderboardData.map((user, index) => (
-          <li key={index}>
-            {user.username} - Total Correct Submissions: {getTotalCorrectSubmissions(user)}
+          <li key={index} className="leaderboard-item">
+            {index === 0 && <img src={goldImage} alt="Gold Medal" style={{ width: '24px', height: '24px', marginRight: '5px' }} />}
+            {index === 1 && <img src={silverImage} alt="Silver Medal" style={{ width: '24px', height: '24px', marginRight: '5px' }} />}
+            {index === 2 && <img src={bronzeImage} alt="Bronze Medal" style={{ width: '24px', height: '24px', marginRight: '5px' }} />}
+            {index>=3 && <span className='index'>{index+1}</span>}
+            <span> {user.username}</span> - Total Correct Submissions: {getTotalCorrectSubmissions(user)}
           </li>
         ))}
       </ul>
